f766bdbf43855fcc7b5b080b951e63e7393150a1
jasonenkristo123 <jasonenkristo@gmail.com>
2025-12-16 16:09:09 +0700

    using localstorage

--- my-project/src/todo.js
+++ my-project/src/todo.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
+let todos = loadTodos();
+renderTodos();
 
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -8,13 +10,39 @@ form.addEventListener("submit", e => {
     const text = input.value.trim();
     if (!text) return;
 
-    const li = document.createElement("li");
-    li.className = "flex justify-between items-center bg-slate-100 px-3 py-2 rounded-md border";
+    const todo = {
+        id: Date.now(), // id unik
+        text
+    };
 
-    li.innerHTML = `<span>${text}</span> <button class="text-white bg-red-500 border px-4 rounded-md">X</button>`
+    todos.push(todo);
+    saveTodos();
+    renderTodos();
 
-    li.querySelector("button").onclick = () => li.remove();
-
-    list.appendChild(li);
     input.value = "";
 })
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}   
+
+function loadTodos() {
+    const data = localStorage.getItem("todos");
+    return data ? JSON.parse(data) : [];
+}
+
+function renderTodos() {
+    list.innerHTML = "";
+
+    todos.forEach(todo => {
+        const li = document.createElement("li");
+        li.className = "flex justify-between items-center bg-slate-100 px-3 py-2 rounded-md border";
+        li.innerHTML = `<span>${todo.text}</span> <button class="text-white bg-red-500 border px-4 rounded-md">X</button>`
+        li.querySelector("button").onclick = () => {
+            todos = todos.filter(t => t.id !== todo.id);
+            saveTodos();
+            renderTodos();
+        };
+        list.appendChild(li);
+    })
+}
